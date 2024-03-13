bf1d8ea08c83d9706a01f1a01feb8daef76c04f3
USER 1 <gchkumar999@gmail.com>
2024-03-13 22:11:51 +0530

    just_pratice

---
+++ three.js
@@ -0,0 +1,25 @@
+
+// create datatype
+
+var Bus="svkdt_travels";
+var price=3500;
+var rating=4.5;
+var discount=30;
+var seats=[15,16,17,18];
+var mydata={
+    name:"Rahul",
+    sal:"56000",
+    city:"vijayawada",
+    age:"46",
+}
+
+// read datatype
+
+console.log(typeof Bus);
+console.log(typeof price);
+console.log(typeof rating);
+console.log(typeof discount);
+console.log(typeof seats);
+console.log(typeof mydata);
+
+
