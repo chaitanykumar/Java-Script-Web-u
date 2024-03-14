c989b3d6fec9256a977c0d65cdd7893b745ee5a0
USER 1 <gchkumar999@gmail.com>
2024-03-14 19:11:13 +0530

    js

---
+++ five.js
@@ -0,0 +1,22 @@
+var a=6;
+var c=7;
+var b=6;
+var d="6";
+console.log(a + "==" + b + ":")
+console.log(a==b);
+console.log("<br>");
+console.log(a + "!=" + b + ":");
+console.log(a!=b);
+console.log("<br>");
+console.log(a + "!=" + c + ":");
+console.log(a!=c);
+console.log("<br>");
+console.log(a + "<" + c + ":");
+console.log(a<c);
+console.log("<br>");
+console.log(a + ">" + c + ":");
+console.log(a>c);
+console.log("<br>");
+console.log(b + "==" + d + ":");
+console.log(b==d);
+console.log("<br>");
