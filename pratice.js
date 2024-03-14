8fe96f2f16e6bc0aa5b418b28025c205d715549f
USER 1 <gchkumar999@gmail.com>
2024-03-14 10:27:16 +0530

    js_pratice89

---
+++ pratice.js
@@ -0,0 +1,16 @@
+// create datatype
+
+var mobile="samsung";
+var price=26000;
+var colour="blue";
+var model=1574;
+var discount=40;
+var location="bangalore";
+
+// read datatype
+
+console.log(typeof mobile);
+console.log(typeof price);
+console.log(typeof model);
+console.log(typeof discount);
+console.log(typeof location);
