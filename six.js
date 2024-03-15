992c5c540ed533acd4a82f35b8deecf65084c900
USER 1 <gchkumar999@gmail.com>
2024-03-15 10:31:01 +0530

    logical_opeators

--- six.js
+++ six.js
@@ -3,6 +3,11 @@ var b=7;
 var c="name";
 var d=0;
 
+
+// [LOGICAL OPERATORS]
+
+// AND operator
+
  console.log(a && c);
 
  console.log(a&&d);
@@ -10,3 +15,16 @@ var d=0;
  console.log((5<7)&&(5<3));
 
  console.log((5<7)&&(1<3));
+
+//  OR operator
+
+console.log((5<7)||(7<3));
+
+console.log((10>20)||(50>60));
+
+
+// NOT operator
+
+console.log(!(5<7));
+
+console.log(!(10>30));
