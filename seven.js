96553a14386628e2f52c962dc9142bfd0daf16f0
USER 1 <gchkumar999@gmail.com>
2024-03-16 11:52:44 +0530

    input-program

--- seven.js
+++ seven.js
@@ -1,8 +1,19 @@
-var esal=45000;
+// var esal=45000;
 
-if (esal>22000){
-    console.log("eligible");
+// if (esal>22000){
+//     console.log("eligible");
+// }
+// else{
+//   console.log("not eligible");
+// }
+
+
+
+var a = 300;
+var b = 200;
+if (a > b) {
+    console.log("true");
 }
-else{
-  console.log("not eligible");
+else {
+    console.log("false");
 }
