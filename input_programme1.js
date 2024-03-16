c3701f6363257cb98dc213eea4110e51d3a5c760
USER 1 <gchkumar999@gmail.com>
2024-03-16 16:44:15 +0530

    pratice

---
+++ input_programme1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+var fnumber = prompt("Enter your first number : ");
+var lnumber = prompt("Enter your last number : ");
+
+
+console.log("Your Full Number is",fnumber,lnumber);
+
+
+
--- seven.js
+++ seven.js
@@ -9,11 +9,21 @@
 
 
 
-var a = 300;
-var b = 200;
-if (a > b) {
-    console.log("true");
+// var a = 300;
+// var b = 200;
+// if (a > b) {
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
+
+
+
+let chai=40000;
+if(chai>50000){
+console.log("Eligible")
 }
-else {
-    console.log("false");
+else{
+    console.log("Not Eligible")
 }
