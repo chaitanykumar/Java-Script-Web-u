ca51af794e496288387880488b179961f578c8d8
USER 1 <gchkumar999@gmail.com>
2024-03-18 18:20:07 +0530

    pratice

--- marks.js
+++ marks.js
@@ -1,10 +1,23 @@
 
-let time =23;
-if (time < 10) {
-  console.log("Good morning");
-} else if (time < 20) {
-    console.log("Good add");
-} else {
-    console.log("Good");
-}
+// let time =23;
+// if (time < 10) {
+//   console.log("Good morning");
+// } else if (time < 20) {
+//     console.log("After");
+// } else {
+//     console.log("Good");
+// }
 
+let score =50;
+if(score<10){
+    console.log("third");
+}
+else if(score<20){
+    console.log("second");
+}
+else if(score<30){
+    console.log("Third");
+}
+else(){
+    console.log("last");
+}
