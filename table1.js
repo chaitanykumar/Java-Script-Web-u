deb31e1064eb01f1956bd8e4bddbe485d06cea4e
USER 1 <gchkumar999@gmail.com>
2024-03-18 23:28:59 +0530

    12-table

--- table1.js
+++ table1.js
@@ -1,5 +1,6 @@
-//print 9 table
+// print 9 table
 
 for(i=1;i<=10;i++){
     console.log(`9 x ${i} = ${i*9}`);
 }
+
---
+++ table2.js
@@ -0,0 +1,6 @@
+
+//print 12 table
+
+for(i=1;i<=10;i++){
+    console.log(`12 x ${i} = ${i*12}`);
+}
