51eb85157f9d81c71e34f53f5816e2acde7fc9cf
USER 1 <gchkumar999@gmail.com>
2024-03-19 11:52:51 +0530

    divisible

---
+++ divisible.js
@@ -0,0 +1,13 @@
+
+//Number that which is divisible by 7
+let Number=89
+       
+    for(i=0;i<=100;i++);
+        
+    
+    if(i%7==0){
+        console.log("divisible by 7");
+    }
+    else{
+        console.log("not divisible by 7");
+    }
--- table2.js
+++ table2.js
@@ -1,6 +1,6 @@
 
 //print 12 table
 
-for(i=1;i<=10;i++){
+for(i=1;i<=20;i++){
     console.log(`12 x ${i} = ${i*12}`);
 }
