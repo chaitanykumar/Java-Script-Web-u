b7e1042e3d2300e153f7d70998312dc55db93aa1
USER 1 <gchkumar999@gmail.com>
2024-03-21 16:43:19 +0530

    prat

---
+++ Object/data.js
@@ -0,0 +1,24 @@
+let user={
+    "id":"101",
+   "name":"kiran",
+   "color":"green",
+   "batch_no":"ac1452",
+
+   "address":{
+    "city":"mysore",
+    "zipcode":"8855-1155",
+   },
+
+   "contact": {
+    "mobile":"7744552136",
+    "email":"kiran74@gmil.com",
+   },
+
+   "services":{
+    "payment":"net-banking",
+    "payment1":"online-payment",
+   } 
+}
+console.log(user.color);
+console.log(user.contact.email);
+console.log(user.services.payment1);
