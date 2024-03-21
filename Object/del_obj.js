1b1e17f400073ceb637f748d41a1c9e9e77931f3
USER 1 <gchkumar999@gmail.com>
2024-03-21 16:04:16 +0530

    prat

---
+++ Object/del_obj.js
@@ -0,0 +1,14 @@
+let emp={
+    id:15211,
+    name:"arjun",
+    fav_color:"blue",
+    email:"arjun99@gmail.com"
+
+}
+
+// delete object
+
+// object and property name(delete emp.email)
+
+delete emp.email;
+console.log(emp);
