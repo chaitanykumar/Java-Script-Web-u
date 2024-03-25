c1f1a7179d30d0421461ed6932a1e0d91d204372
USER 1 <gchkumar999@gmail.com>
2024-03-25 15:44:40 +0530

    prat

--- Function/nine.js
+++ Function/nine.js
@@ -1,5 +1,14 @@
+
+// create once use n number of times
+
 function add(a,b){
     return a+b
 }
 var result=add(50,30);
+console.log(result);
+
+var result=add(200,300);
+console.log(result);
+
+var result=add(500,500);
 console.log(result);
