e6296638adca0c6d63cb2c40330e61be48f92ed9
USER 1 <gchkumar999@gmail.com>
2024-03-25 12:35:30 +0530

    prat

--- Function/seven.js
+++ Function/seven.js
@@ -1,6 +1,11 @@
+
+// create function once invoke n number of times 
+
 function emp_Details(id,name,sal){   //Parameters
     console.log(id); //Printing the valus
     console.log(name); //Printing the valus
-    console.log(sal); //Printing the valus
+    console.log(sal+5000); //Printing the valus
 }
 emp_Details(101,"Rahul",75000) // Arguments
+emp_Details(102,"Aravind",65000) // Arguments
+emp_Details(103,"kumar",55000) // Arguments
