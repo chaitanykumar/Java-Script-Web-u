abed4c26f5f6c660c522333c828bb9fb3c683ac9
USER 1 <gchkumar999@gmail.com>
2024-03-25 12:28:57 +0530

    prat

---
+++ Function/seven.js
@@ -0,0 +1,6 @@
+function emp_Details(id,name,sal){   //Parameters
+    console.log(id); //Printing the valus
+    console.log(name); //Printing the valus
+    console.log(sal); //Printing the valus
+}
+emp_Details(101,"Rahul",75000) // Arguments
