9063adaf82df39e960d4f03dd95c56c96cd4da33
USER 1 <gchkumar999@gmail.com>
2024-03-28 20:31:04 +0530

    prat

--- FatArrow/sub.js
+++ FatArrow/sub.js
@@ -1,5 +1,9 @@
 // Implicite Fat Arrow Function
 
+// let sub=(a,b)=>a-b
+// let result=sub(500,250);
+// console.log(result);
+
 let sub=(a,b)=>a-b
-let result=sub(500,250);
+let result=sub(50,25)
 console.log(result);
