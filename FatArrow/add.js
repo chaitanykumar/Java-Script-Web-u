5b4a8f9e5875146590a2da2a7a2960fbbde2f44e
USER 1 <gchkumar999@gmail.com>
2024-03-28 19:54:03 +0530

    prat

---
+++ FatArrow/add.js
@@ -0,0 +1,12 @@
+// Fat Arrow Function with return
+
+// let add=(a,b)=>{
+//     return a+b
+// }
+// let r1=add(30,20);
+// console.log(r1);
+
+//Implicite Fat Arow Function
+let add=(a,b)=>a+b
+let r1=add(100,200);
+console.log(r1);
