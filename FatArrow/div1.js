9a562ab03d634a3e06b105a1dafaf70bd4b15148
USER 1 <gchkumar999@gmail.com>
2024-03-30 23:17:00 +0530

    prat

--- FatArrow/div1.js
+++ FatArrow/div1.js
@@ -1,5 +1,10 @@
 // Implicite Fat Arrow Function
 
-let div1=(a,b)=>a/b;
-let result =div1(500,250);
+// let div1=(a,b)=>a/b;
+// let result =div1(500,250);
+// console.log(result);
+
+
+let div=(a,b)=>a/b;
+let result=div(50,80);
 console.log(result);
