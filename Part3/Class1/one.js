e998643654b057fefc45362d4d64c79537138735
USER 1 <gchkumar999@gmail.com>
2024-04-02 23:29:34 +0530

    prat

---
+++ Part3/Class1/one.js
@@ -0,0 +1,16 @@
+class Account{
+ac_id;
+ac_name;
+ac_bal;
+open_account(){}
+check_balance(){}
+deposit_ammount(){}
+close_account(){}
+}
+let a1=new Account();
+let a2=new Account();
+let a3=new Account();
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
