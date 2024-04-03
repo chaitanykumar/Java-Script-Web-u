807149c9eeb4d86a391af6cdac3d08109bca0063
USER 1 <gchkumar999@gmail.com>
2024-04-03 11:13:30 +0530

    prat

---
+++ Part3/Class1/three.js
@@ -0,0 +1,13 @@
+class Account{
+    acc_bal=200;
+    dep_ammount(){
+        console.log("Deposit ammount successfully ");
+    }
+}
+let a1=new Account();
+let a2=new Account();
+console.log(a1);
+console.log(a2);
+
+a1.dep_ammount();
+a2.dep_ammount();
