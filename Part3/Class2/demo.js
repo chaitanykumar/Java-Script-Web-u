4405f7d592bfcc1ebc8e19e1fe4b4ac3cc868ed2
USER 1 <gchkumar999@gmail.com>
2024-04-04 21:16:50 +0530

    prat

--- Part3/Class2/demo.js
+++ Part3/Class2/demo.js
@@ -17,13 +17,19 @@ class Account{
         this.acc_Bal=this.acc_Bal-amount
     }
     get_Bal(){
-        return this.acc_Bal()
+        return this.acc_Bal
     }
     close_account(){}
 } 
           
-let a1=new Account(101,"Rahul",6000)
-console.log(a1);
+ let a1=new Account(101,"Rahul",6000)
+console.log("base money",a1);
 
 a1.depAmount(7000);
-console.log(a1);
+console.log("deposited",a1);
+
+// let gb=a1.get_Bal();
+// console.log("get balance",gb);
+
+let Rahul=a1.get_Bal();
+console.log("get balance",Rahul);
