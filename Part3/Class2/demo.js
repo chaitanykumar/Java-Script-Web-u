f5ebc15ebd01752db4f6d2262b3a24b6d170e9a3
USER 1 <gchkumar999@gmail.com>
2024-04-04 21:02:21 +0530

    prat

---
+++ Part3/Class2/demo.js
@@ -0,0 +1,29 @@
+class Account{   
+    acc_Id;
+    acc_Name;
+    acc_Bal=0;
+    constructor(id,name,amount){
+        this.acc_Id=id;
+        this.acc_Name=name;
+        this.acc_Bal=amount;              
+    }
+    open_Account(){
+        console.log("Account opened successfully");
+    }
+    depAmount(amount){
+        this.acc_Bal=this.acc_Bal+amount;
+    }
+    with_Drawl(){                                                   
+        this.acc_Bal=this.acc_Bal-amount
+    }
+    get_Bal(){
+        return this.acc_Bal()
+    }
+    close_account(){}
+} 
+          
+let a1=new Account(101,"Rahul",6000)
+console.log(a1);
+
+a1.depAmount(7000);
+console.log(a1);
