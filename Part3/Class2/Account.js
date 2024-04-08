ed1766a07c56ddfcf2bce6984ff1ea89616a9bd9
USER 1 <gchkumar999@gmail.com>
2024-04-08 19:24:13 +0530

    prat

---
+++ Part3/Class2/Account.js
@@ -0,0 +1,10 @@
+class Account{
+    constructor(){
+        console.log("Good Morning");
+        console.log("Good Aternoon");
+        console.log("Good Evening");
+        console.log("Good Night");
+    }
+}
+new Account()
+new Account()
