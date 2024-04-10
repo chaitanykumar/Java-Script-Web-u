67d7d76a1da840236591fc60d5cf650f841b7231
USER 1 <gchkumar999@gmail.com>
2024-04-10 20:04:06 +0530

    prat

---
+++ JSES6/Array_Methods2/one.js
@@ -0,0 +1,10 @@
+ let prices=[95,25,300,255,275,455,160]
+
+// createing new prices values - by adding +1
+let new_prices=[]
+for(price of prices){
+    new_prices.push(price+1)
+}
+console.log(new_prices);
+
+
