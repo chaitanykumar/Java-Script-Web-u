ad5c1212ec55e71547fee10ad64743287f0c3e09
USER 1 <gchkumar999@gmail.com>
2024-04-10 19:20:29 +0530

    prat

---
+++ JSES6/Array_Methods/one.js
@@ -0,0 +1,30 @@
+var enames=["Rahul","Arjun","Modi","Kumar"];
+// //itrate array - enames using for loop, while loop, dowhile loop, for..of
+
+//1.for-loop
+
+for(let i=0;i<=enames.length-1;i++){
+    console.log(enames[i]);
+}
+
+//2.while-loop
+
+var i=0;
+while(i<=enames.length-1){
+ console.log(enames[i])
+ i++
+}
+ 
+//3.do-while-loop
+
+var i=0;
+do{
+    console.log(enames[i])
+    i++
+}while(i<=enames.length-1){
+    console.log("using for ..of");
+}
+
+for(ename of enames){
+    console.log(ename);
+}
