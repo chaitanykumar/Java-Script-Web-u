7be9ce00737472df27be7b1c872735b950cbad51
USER 1 <gchkumar999@gmail.com>
2024-04-13 11:37:54 +0530

    prat

--- JSES6/Array_Methods2/three.js
+++ JSES6/Array_Methods2/three.js
@@ -1,15 +1,7 @@
-// let prices=[100,250,450,950,623]
-
-// //creating new prices value by addding +1 by using forEach
-
-// let new_prices=prices.forEach((price)=>{
-//    return price +1
-// })
-// console.log(new_prices)
-
 let prices=[98,198,298,398]
 
 //create new Prices values - by adding plus + 1
+//forEach doesn't written new array for that we are using map method
 
 let new_Prices=prices.forEach((price)=>{
     return price +1
