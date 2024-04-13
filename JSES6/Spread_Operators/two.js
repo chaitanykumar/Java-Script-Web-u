0feb2951ffe0faea9c934bb15f38a815079b7faa
USER 1 <gchkumar999@gmail.com>
2024-04-13 21:15:03 +0530

    prat

---
+++ JSES6/Spread_Operators/two.js
@@ -0,0 +1,10 @@
+let ids=[101,102,103,104,105]
+
+//Create new array based on exitising array
+ 
+let new_ids=[106,107,108,109]
+for(id of ids){
+    new_ids.push(id)
+}
+console.log(ids);
+console.log(new_ids);
