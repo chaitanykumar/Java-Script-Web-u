5164b6fca8d56324769f6d9a3ab65fc9e31189db
USER 1 <gchkumar999@gmail.com>
2024-04-14 10:56:35 +0530

    prat

---
+++ JSES6/Spread_Operators/three.js
@@ -0,0 +1,11 @@
+let ids=[101,102,103,104]
+
+//Create new array based on existing array
+
+let new_ids=[...ids,105,106,107]
+
+//The spread operator expressions represents as ...
+//Extract the array and create the new array
+
+console.log(ids);
+console.log(new_ids);
