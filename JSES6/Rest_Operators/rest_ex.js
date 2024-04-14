65eccf379338e402fd91bf14ed4a08a1a077e08e
USER 1 <gchkumar999@gmail.com>
2024-04-14 16:41:28 +0530

    prat

---
+++ JSES6/Rest_Operators/rest_ex.js
@@ -0,0 +1,17 @@
+// function add(a,...b){
+//     console.log("a=",a,"and b=",b)
+// }
+// add(10)
+// add(10,20)
+// add(10,20,30)
+// add(10,20,30,40)
+// add(10,20,30,40,50)
+
+function add(a,...b){
+console.log("a=",a,"and rest of the numbers=",b)
+}
+add(10)
+add(10,20)
+add(10,20,30)
+add(10,20,30,40)
+add(10,20,30,40,50)
