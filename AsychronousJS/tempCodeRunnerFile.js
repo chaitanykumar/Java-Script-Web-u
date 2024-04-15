81c800dfe166184b20fd241ab135e9cfc4e4eae2
USER 1 <gchkumar999@gmail.com>
2024-04-15 19:39:43 +0530

    practice

---
+++ AsychronousJS/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    console.log("R1");
+},7000)
+
+setTimeout(()=>{
+    console.log("R2");
+},2000)
+
+setTimeout(()=>{
+    console.log("R3");
+},3000);
+
+setTimeout(()=>{
+    console.log("R4");
+},1000)
---
+++ AsychronousJS/two.js
@@ -0,0 +1,18 @@
+
+// setTimeout executes the code only once after the specified delay of time 
+
+setTimeout(()=>{
+    console.log("R1");
+},7000)
+
+setTimeout(()=>{
+    console.log("R2");
+},2000)
+
+setTimeout(()=>{
+    console.log("R3");
+},3000);
+
+setTimeout(()=>{
+    console.log("R4");
+},1000)
